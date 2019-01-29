12db3810d2ae7d3e7eacafcac7a55c1e7dabc488
Saranyu Ngaosri <39403998+ouujj@users.noreply.github.com>
2019-01-29 17:22:49 +0700

    Complete

--- Mutual information of this channel.py
+++ Mutual information of this channel.py
@@ -1,50 +1,70 @@
 import matplotlib.pyplot as plt
 import math
 
+q=[]
+nEr=(int)(input("Number of Error: "))
+for i in range(nEr):
+    q.append(input("ERROR : "))
+    q[i]=float(q[i])
 
-q=input("ERROR : ")
-q=float(q)
-px0=0.01
-PX=[]
-IXY=[]
 
-for i in range (99):
-    px1=1-px0
+
+
+def Entopy (ci,q):
+    Color=['blue','green','orange','yellow','red']
+    PX=[]
+    IXY=[]
+    px0=0.01
     Q=1-q
-
-    py0=px0*Q+px1*q
-    py1=px0*q+px1*Q
-
-    #H(X,Y)
-    Hxy = -1*px0*Q*math.log(px0*Q,2)-q*px1*math.log(px1*q,2)
-    #H(X)
-    Hx = -1*px1*math.log(px1,2)-px0*math.log(px0,2)
-    #H(Y)
-    Hy = -1*py1*math.log(py1,2)-py0*math.log(py0,2)
-    #I(X;Y)
-    Ixy=Hx+Hy-Hxy
+    PX.append(0)
+    IXY.append(0)
     
-    PX.append(px0)
-    IXY.append(Ixy)
-    px0+=0.01
+    #H(X|Y)
+    Hxy = -1*Q*math.log(Q,2)-q*math.log(q,2)
+    for i in range (99):
+        px1=1-px0
+      
 
-    #print(Hx , Hy , Hxy ,Ixy)
-for i in range(20):
-    print()
-    print("P(x): ",end="")
-    for j in range(5):
-        print(str(PX[j*i+i])+",",end="")
-print()         
-print("=======================================================================================================")      
-for i in range(20):
-    print()
-    print("I(x;y): ",end="")
-    for j in range(5):
-        print(str(IXY[j*i+i])+",",end="")    
-plt.plot(PX, IXY, color='orange')
+        py0=px0*Q+px1*q
+        py1=px0*q+px1*Q
+         #H(X)
+        Hx = -1*px1*math.log(px1,2)-px0*math.log(px0,2)
+        #H(Y)
+        Hy = -1*py1*math.log(py1,2)-py0*math.log(py0,2)
+        #I(X;Y)
+        Ixy=Hy-Hxy
+    
+        PX.append(px0)
+        IXY.append(Ixy)
+        px0+=0.01
+
+        #print(Hx , Hy , Hxy ,Ixy)
+
+    PX.append(1)
+    IXY.append(0)
+    print("Error = "+str(q))
+    for i in range(19):
+        print()
+        print("P(x): ",end="")
+        for j in range(5):
+            print(str(PX[(5*i)+j])+",",end="")
+    print()             
+    for i in range(19):
+        print()
+        print("I(x;y): ",end="")
+        for j in range(5):
+            print(str(IXY[(5*i)+j])+",",end="")    
+    plt.plot(PX, IXY, color=Color[ci])
+    print()         
+    print("=======================================================================================================")
+    
+for i in range(nEr):
+    Entopy(i,q[i])
+    
+
 plt.xlabel('P(X)')
 plt.ylabel('I(X;Y)')
-plt.title('Mutual information of channel with error'+str(q))
+plt.title('Mutual information of channel')
 plt.show()
 
 
